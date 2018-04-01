e1adaf62c89ca2d2504a5eda77a813286980208e
tejasambalia <tejas.ambalia1994@gmail.com>
2018-04-01 11:10:52 +0530

    fixed copy WO

--- includes/copywo.php
+++ includes/copywo.php
@@ -4,10 +4,15 @@ unset($_POST);
 if(isset($_data['id'])&&$_data['id']>0&&is_numeric($_data['id'])){
 	$data = get_wo_detail_by_id($_data['id']);
 	$data1 = $data;
-	unset($data1['id']);
 	$data2 = insertlog();
-	$tabledata = array_merge($data1, $data2);
-	insert('costing_tool', $tabledata, $dbh);
+	//$tabledata = array_merge($data1, $data2);
+	$wo_id = insert('work_order', $data2);
+	for($i = 0; $i < count($data1); $i++){
+		unset($data1[$i]['id']);
+		$data1[$i]['work_order_id'] = $wo_id;
+		$tabledata = array_merge($data1[$i], $data2);
+		insert('work_order_items', $tabledata, $dbh);
+	}
 
 	setMessage('WO #'.$_data['id'].' copied successfully!', 'success');
 }
